a5651f90cd425184eb0759e7ae31b0ad8046effe
s2wuberrycake <s2wuberrycake@gmail.com>
2025-05-21 04:30:32 +0800

    -

--- client/src/components/ui/button.jsx
+++ client/src/components/ui/button.jsx
@@ -20,7 +20,7 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
-        main: "bg-chart-1 text-primary dark:text-secondary hover:bg-chart-2/90",
+        main: "bg-chart-1 text-primary dark:text-background hover:bg-chart-2/90",
       },
       size: {
         default: "h-9 px-9 py-5 has-[>svg]:px-3",
